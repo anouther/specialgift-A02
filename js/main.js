08db2c4ab650d442fdd52ffecd187ffca080c91d
anouther <155570107+anouther@users.noreply.github.com>
2025-02-14 02:35:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Bunga yg Cantik, Untuk Si Cantik').split('');
+        const titles = ('May this little gift remind you of how deeply I cherish you, today and always.').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Sayang, cantiknya tolong dikondisikan ya, jantung abang ga kuat liatnya.";
+            subtitle.innerText = "If the stars could whisper, they would tell you how often I think of you. If the wind could sing, it would hum the melody of my heart every time you’re near. But since neither can speak, let this flower carry my love to you. Every petal of this flower carries a whisper of my love for you—soft, pure, and endless. Just like this bloom, my feelings for you will never fade but only grow more beautiful with time. ";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
